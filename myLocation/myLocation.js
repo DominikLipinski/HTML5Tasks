5a7f88bf282f36b61f6bf19ee8f84cbb368d92d6
Dominik Lipinski <maboonga@gmail.com>
2017-09-20 17:42:55 +0200

    error handler

--- myLocation/myLocation.js
+++ myLocation/myLocation.js
@@ -2,7 +2,7 @@ window.onload = getMyLocation;
 
 function getMyLocation() {
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(displayLocation);
+        navigator.geolocation.getCurrentPosition(displayLocation, displayError);
     }else{
         alert("No geolocation support here :-(");
     }
@@ -15,3 +15,44 @@ function displayLocation(position){
     var div = document.getElementById("location");
     div.innerHTML = "Your coordinates are: latitude: "+latitude+", longitude: "+longitude;
 }
+
+function displayError(error){
+    var errorTypes = {
+        0: "Unknown Error",
+        1: "Acces not allowed",
+        2: "Location not available",
+        3: "Time limit exceeded"
+    };
+
+    var errorMessage = errorTypes[error.code];
+    if(error.code==0||error.code==2){
+        errorMessage=errorMessage+" "+error.message;
+    }
+    var div = document.getElementById("location");
+    div.innerHTML = errorMessage;
+}
+
+function computeDistance(startCoords, destCoords){
+    var startLatRads = deegreesToRadians(startCoords.latitude),
+        startLongRads = deegreesToRadians(startCoords.longitude),
+        destLatRads = deegreesToRadians(destCoords.latitude),
+        destLongRads = deegreesToRadians(destCoords.longitude),
+        Radius = 6371,
+        distance = Math.acos(Math.sin(startLatRads)*Math.sin(destLatRads)+
+                Math.cos(startLongRads)*Math.cos(destLongRads)*
+                Math.cos(startLongRads - destLongRads)) * Radius;
+
+    return distance;
+}
+
+function deegreesToRadians(degrees){
+    var radians = (degrees *Math.PI)/180;
+    return radians;
+}
+
+var secretLocation = {
+    latitude: 50.288962,
+    longitude: 18.659609
+};
+
+
